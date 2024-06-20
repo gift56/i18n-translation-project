a5b9c54dfad69498c7a1dfec9c5438e5e147acea
Efe G <98314362+gift56@users.noreply.github.com>
2024-06-20 13:13:43 -0700

    working on translation

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,11 +1,20 @@
-import Image from 'next/image'
+import Image from "next/image";
+import initTranslations from "../i18n";
 
-export default function Home() {
+interface HomeProps {
+  params: {
+    locale: string;
+  };
+}
+
+export default async function Home({ params: { locale } }: HomeProps) {
+  const { t } = await initTranslations(locale, ["home", "common"]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
+          {t("get_started")}&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
@@ -15,7 +24,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            {t("common:by")}{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -47,13 +56,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
+            {t("docs")}{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+            {t("doc_content")}
           </p>
         </a>
 
@@ -64,13 +73,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
+            {t("learn")}{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            {t("learn_content")}
           </p>
         </a>
 
@@ -81,13 +90,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
+            {t("templete")}{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
+            {t("templete_content")}
           </p>
         </a>
 
@@ -98,16 +107,16 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
+            {t("deploy")}{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            {t("deploy_content")}
           </p>
         </a>
       </div>
     </main>
-  )
+  );
 }
---
+++ i18nConfig.ts
@@ -0,0 +1,6 @@
+const i18nConfig = {
+  locales: ["en", "fr", "it", "de"] as const,
+  defaultLocale: "en" as const,
+};
+
+export default i18nConfig;
